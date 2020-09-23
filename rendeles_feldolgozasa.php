8f4fa46c649ff3db89854cd84c762ea104c0d9f8
Danyi László <laszlo.danyi.81@gmail.com>
2020-09-23 09:26:23 +0200

    kezdet

---
+++ rendeles_feldolgozasa.php
@@ -0,0 +1,15 @@
+
+<html>
+  <head>
+    <meta charset="utf-8">
+    <title>Bob autóalkatrészek - Rendelési eredmények</title>
+  </head>
+  <body>
+    <h1>Bob autóalkatrészek</h1>
+    <h2>Rendelési eredmények</h2>
+    <?php
+    echo '<p>Rendleés feldolgozva.</p>' 
+    ?>
+
+  </body>
+</html>
